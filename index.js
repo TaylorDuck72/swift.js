6d262e06e1529a0c787aa6b6085a2b2e1f7c42b6
BuildTools <unconfigured@null.spigotmc.org>
2022-07-15 00:08:29 +0800

    added index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+class Swifts {
+    constructor(str) {
+        this.str = str;
+    }
+    Swift() {
+        let returnText = `${this.str} sweefed`
+        return returnText;
+    }
+}
+
+module.exports = Swifts
